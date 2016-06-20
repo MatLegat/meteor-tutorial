a671a10430ba6c2499dc0d5df7fabef938edffba
Matteus Legat <matteuslegat@gmail.com>
2016-06-20 00:19:22 -0300

    Created form with add option

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,35 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+import template from './todosList.html';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    })
+  }
+
+  addTask(newTask) {
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    this.newTask = '';
+  }
+}
+
+export default angular.module('todosList', [angularMeteor])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
