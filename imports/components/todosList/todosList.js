2f3bd19e3e490a2d19469c3399cc7087214b23c0
Matteus Legat <matteuslegat@gmail.com>
2016-07-03 18:05:51 -0300

    Filter completed tasks

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -7,13 +7,32 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
-        return Tasks.find({}, {
+        const selector = {};
+
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        // Show newest tasks at the top
+        return Tasks.find(selector, {
           sort: {
             createdAt: -1
           }
         });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
       }
     })
   }
