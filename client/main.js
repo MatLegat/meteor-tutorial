588185a01a9fa7e9c595595bb8e5d7baebf274b3
Matteus Legat <matteuslegat@gmail.com>
2016-07-04 15:30:13 -0300

    Includes login system

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
 
 angular.module('simple-todos', [
   angularMeteor,
-  todosList.name
+  todosList.name,
+  'accounts.ui'
 ]);
 
 function onReady() {
--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
 import template from './todosList.html';
 
@@ -33,6 +34,9 @@ class TodosListCtrl {
             $ne: true
           }
         }).count();
+      },
+      currentUser() {
+        return Meteor.user();
       }
     })
   }
@@ -40,7 +44,9 @@ class TodosListCtrl {
   addTask(newTask) {
     Tasks.insert({
       text: newTask,
-      createdAt: new Date
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
 
     this.newTask = '';
